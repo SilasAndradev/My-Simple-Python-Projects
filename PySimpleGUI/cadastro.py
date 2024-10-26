f464a58da865499c0493da3b0fa8df1723c6267c
Silas <silasandrade.code@gmail.com>
2024-10-26 14:45:36 +0000

    main

--- PySimpleGUI/cadastro.py
+++ PySimpleGUI/cadastro.py
@@ -6,10 +6,18 @@ layout = [
     [sg.Text('Primeiro número'),sg.Input(key='prinum')],
     [sg.Text('Segundo número'),sg.Input(key='segnum')],
     [sg.Checkbox('Lembrar a conta?')],
+    [sg.Button('Limpar campos')]
     [sg.Button('Encerrar')]
 ]
 
 # Janela
 janela = sg.Window('Tela de Soma', layout)
+
 # Ler os eventos
-while T
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED or eventos == 'Encerrar':
+        break
+    elif eventos == 'Limpar campos':
+        janela['prinum'].update('')
+        janela['segnum'].update('')
