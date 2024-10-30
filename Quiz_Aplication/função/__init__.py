1d53ab71db818f180fe72d4c01da61995ae0b0a4
Silas <silasandrade.code@gmail.com>
2024-10-30 00:05:07 +0000

    main

--- Quiz_Aplication/função/__init__.py
+++ Quiz_Aplication/função/__init__.py
@@ -1,10 +1,30 @@
+from time import sleep
+def tempo():
+    j = 0
+    for j in range(59, 0, -1):
+        sleep(1)
+        j+=1
+        print(j)
+    
+
+
 def quiz(questions):
-    for perguntas, resposta in questions:
+    if not questions:
+        print("Nenhuma pergunta fornecida.")
+        return
+    for perguntas, resposta in questions.items():
         print(f'{perguntas}')
+        if not resposta or not isinstance(resposta, list):
+            print("Opções de resposta inválidas fornecidas.")
+            continue
         print(sorted(resposta))
         resposta_correta = resposta[0]
-        suaResp = str(input(''))
+        try:
+            suaResp = str(input(''))
+        except EOFError:
+            print("Ocorreu um erro de entrada.")
+            continue
         if suaResp == resposta_correta:
-            print('Reposta correta')
+            print('Resposta correta')
         else:
-            print('Respota incorreta')
+            print('Resposta incorreta')
--- Quiz_Aplication/quiz.py
+++ Quiz_Aplication/quiz.py
@@ -14,5 +14,5 @@ questions = {
         ],
 }
 
-quiz(questions)
+tempo()
 
