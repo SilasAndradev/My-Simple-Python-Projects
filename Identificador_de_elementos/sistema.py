479bf2fe50bcfc2845c80fc2550a1dcc8a90100a
Silas <silasandrade.code@gmail.com>
2024-11-15 00:08:34 -0300

    Update sistema.py

--- Identificador_de_elementos/sistema.py
+++ Identificador_de_elementos/sistema.py
@@ -1,15 +1,3 @@
-"""
-Esse programa vai ler um arquivo txt, com as informções 
-de alguns elementos qúimicos
-E faz as seguintes coisas:
-1. Mostra os nomes dos elementos químicos
-    que estão no arquivo txt
-2. Mostra os nomes e as propriedades dos elementos químicos
-   que estão no arquivo txt
-3. Faz um quiz com tempo em que o programa mostra as propriedades
-    dos elementos químicos e o usuário tem que descobrir o seu nome
-"""
-
 from função.arquivo import * 
 from função.quiz import *
 import threading as th
@@ -22,6 +10,7 @@ jogo = game
 stop_event = th.Event()
 
 arquivo = "Identificador_de_elementos/elementos.txt"
+
 
 # Se o não arquivo existe, ele cria 
 if not arquivoExiste(arquivo):
